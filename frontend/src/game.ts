43b1c2b74698b97972a899fd65a91241fbb8e568
mahahahad <mahad.abd2003@gmail.com>
2025-09-22 23:38:42 +0400

    feat: add score system
    also adds ball serve side swap on player score

--- frontend/src/game.ts
+++ frontend/src/game.ts
@@ -8,13 +8,16 @@ const PADDLE_WIDTH = 25;
 const BALL_HEIGHT = 25;
 const BALL_WIDTH = 25;
 
+let p1Score = 0;
+let p2Score = 0;
+
 canvas.height = BOARD_HEIGHT;
 canvas.width = BOARD_WIDTH;
 canvas.style.backgroundColor = "black";
 
 requestAnimationFrame(update);
 
-interface Coordinate {
+interface CanvasShape {
   x: number;
   y: number;
   width: number;
@@ -22,8 +25,8 @@ interface Coordinate {
 }
 
 let ball = {
-  x: canvas.width / 2 - BALL_WIDTH,
-  y: canvas.height / 2 - BALL_HEIGHT,
+  x: canvas.width / 2 - BALL_WIDTH / 2,
+  y: canvas.height / 2 - BALL_HEIGHT / 2,
   width: BALL_WIDTH,
   height: BALL_HEIGHT,
   velocity: { x: 2, y: 1 },
@@ -31,7 +34,7 @@ let ball = {
 
 let paddle1 = {
   x: 16,
-  y: canvas.height / 2 - PADDLE_HEIGHT,
+  y: canvas.height / 2 - PADDLE_HEIGHT / 2,
   width: PADDLE_WIDTH,
   height: PADDLE_HEIGHT,
   velocityY: 0,
@@ -39,13 +42,13 @@ let paddle1 = {
 
 let paddle2 = {
   x: canvas.width - PADDLE_WIDTH - 16,
-  y: canvas.height / 2 - PADDLE_HEIGHT,
+  y: canvas.height / 2 - PADDLE_HEIGHT / 2,
   width: PADDLE_WIDTH,
   height: PADDLE_HEIGHT,
   velocityY: 0,
 };
 
-function detectCollision(a: Coordinate, b: Coordinate) {
+function detectCollision(a: CanvasShape, b: CanvasShape) {
   return (
     a.x < b.x + b.width && //a's top left corner doesn't reach b's top right corner
     a.x + a.width > b.x && //a's top right corner passes b's top left corner
@@ -55,11 +58,26 @@ function detectCollision(a: Coordinate, b: Coordinate) {
 }
 
 function update() {
+  // Remove previous content
   ctx.clearRect(0, 0, BOARD_WIDTH, BOARD_HEIGHT);
   ctx.fillStyle = "white";
+
+  // Top Stroke
   ctx.fillRect(0, 0, canvas.width, 1);
+
+  // Bottom Stroke
   ctx.fillRect(0, canvas.height - 1, canvas.width, 1);
 
+  // Center Line
+  ctx.lineWidth = 1;
+  ctx.setLineDash([8, 8]);
+  ctx.strokeStyle = "white";
+  ctx.beginPath();
+  ctx.moveTo(canvas.width / 2 - 1, 0);
+  ctx.lineTo(canvas.width / 2 - 1, canvas.height);
+  ctx.stroke();
+
+  // Paddles
   let p1NewPos = {
     x: paddle1.x,
     y: paddle1.y + paddle1.velocityY,
@@ -84,36 +102,58 @@ function update() {
       paddle2.height
     );
   else ctx.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
+
+  // Ball Collision
   ball.x += ball.velocity.x;
   ball.y += ball.velocity.y;
   if (ball.y <= 0 || ball.y + ball.height >= canvas.height) {
-    // if ball touches top or bottom of canvas
-    ball.velocity.y *= -1; //reverse direction
+    ball.velocity.y *= -1;
   }
   if (detectCollision(ball, paddle1)) {
     if (ball.x <= paddle1.x + paddle1.width) {
-      //left side of ball touches right side of paddle 1 (left paddle)
-      ball.velocity.x *= -1; // flip x direction
+      ball.velocity.x *= -1;
     }
   } else if (detectCollision(ball, paddle2)) {
     if (ball.x + ball.width >= paddle2.x) {
-      //right side of ball touches left side of paddle 2 (right paddle)
-      ball.velocity.x *= -1; // flip x direction
+      ball.velocity.x *= -1;
     }
   }
+
+  // TODO: Update reset logic
   if (ball.x + ball.velocity.x >= canvas.width) {
-    ball.x = canvas.width / 2 - ball.width;
-    ball.y = canvas.height / 2 - ball.height;
+    // Manual Reset
+    ball.x = canvas.width / 2 - ball.width / 2;
+    ball.y = canvas.height / 2 - ball.height / 2;
+    ball.velocity.x = 2;
+    ball.velocity.y = 1;
+    p1Score++;
   } else if (ball.x + ball.velocity.x <= 0) {
-    ball.x = canvas.width / 2 - ball.width;
-    ball.y = canvas.height / 2 - ball.height;
+    // Manual Reset
+    ball.x = canvas.width / 2 - ball.width / 2;
+    ball.y = canvas.height / 2 - ball.height / 2;
+    ball.velocity.x = -2;
+    ball.velocity.y = 1;
+    p2Score++;
   }
+
+  // Ball Draw
   ctx.fillRect(
     (ball.x += ball.velocity.x),
     (ball.y += ball.velocity.y),
     ball.width,
     ball.height
   );
+
+  // Score
+  const FONT_SIZE = 128;
+  const PADDING = 36;
+  ctx.fillStyle = "rgba(49, 49, 49, 1)";
+  ctx.font = `${FONT_SIZE}px Workbench`;
+  ctx.fillText(p1Score.toString(), canvas.width / 4, FONT_SIZE + PADDING);
+  ctx.fillText(p2Score.toString(), (3 * canvas.width) / 4, FONT_SIZE + PADDING);
+
+  if (p1Score >= 5 || p2Score >= 5) return;
+  // Draw again next frame
   requestAnimationFrame(update);
 }
 
