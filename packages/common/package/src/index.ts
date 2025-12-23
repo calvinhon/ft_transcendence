c6b05ca0a1ea5d2d9f3e5d4d31e558863f95d4d8
Nguyen Hoach <nthoach@gmail.com>
2025-12-23 14:39:26 +0400

    cleanup: Remove build artifacts from packages/common

--- packages/common/package/src/index.ts
+++
@@ -1,8 +0,0 @@
-// packages/common/src/index.ts
-export * from './responses';
-export * from './types';
-export * from './health';
-export * from './logger';
-export * from './server';
-export * from './database';
-export * from './middleware';
