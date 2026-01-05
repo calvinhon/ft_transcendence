86b12bf79c9f1340da238a9eae070da2cc096dce
mahahahad <mahad.abd2003@gmail.com>
2026-01-05 08:33:19 +0400

    fix: add retries in getting vault secret
    needed because the vault is unsealed later than this is called

--- packages/common/src/session.ts
+++ packages/common/src/session.ts
@@ -17,12 +17,23 @@ export const sessionSecret = fp(async (fastify) => {
 	redisClient.on('error', (err) => { console.log('Redis Client Error: ', err); })
 
 	let response = null;
-	try {
-		response = await axios.get(`${process.env.VAULT_ADDR}/v1/kv/data/Client_Session`, { headers: { 'X-Vault-Token': process.env.VAULT_TOKEN } });
-		console.log("Successfully retrieved secret key for signing session tokens.");
-	} catch (err: any) {
-		console.log("Error encounter while attempting to retrieve session secret: ", err.message);
-		process.exit(1);
+	let retries = 5;
+	while (retries > 0) {
+		try {
+			response = await axios.get(`${process.env.VAULT_ADDR}/v1/kv/data/Client_Session`, {
+				headers: { 'X-Vault-Token': process.env.VAULT_TOKEN }
+			});
+			console.log("Successfully retrieved secret key for signing session tokens.");
+			break;
+		} catch (err: any) {
+			console.log(`Error encounter while attempting to retrieve session secret (Retries left: ${retries}): `, err.message);
+			retries--;
+			if (retries === 0) {
+				console.log("Failed to retrieve session secret after multiple attempts.");
+				process.exit(1);
+			}
+			await new Promise(resolve => setTimeout(resolve, 2000));
+		}
 	}
 	const secretKey = response?.data.data.data.Secret;
 
